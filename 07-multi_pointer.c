98955ca5aa8dc11611233bce885e0a3f233d29a0
root <yi2.li@intel.com>
2021-02-09 17:29:19 +0800

    解决分配内存空间不足的bug

--- 07-multi_pointer.c
+++ 07-multi_pointer.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
 int print_array(char **p_array, int len)
@@ -38,10 +40,28 @@ int sort_array(char **p_array, int len)
 
 int split_str(const char *str, char **p_array, int *rows)
 {
-  int i =0;
+  int i =0, ret_val=0;
   char *str_ptr = str;
   char *tmp_ptr = str;
+  if (str == NULL)
+  {
+    ret_val = -1;
+    printf("func split_str() error: (str == NULL)");
+    return ret_val;
+  }
   
+  if (str_ptr == NULL)
+  {
+    ret_val = -1;
+    printf("func split_str() error: (str_ptr == NULL)");
+    return ret_val;
+  }
+  if (tmp_ptr == NULL)
+  {
+    ret_val = -1;
+    printf("func split_str() error: (tmp_ptr == NULL)");
+    return ret_val;
+  }
   while(*str_ptr!= '\0')
   {
     if (*str_ptr == ',')
@@ -56,9 +76,11 @@ int split_str(const char *str, char **p_array, int *rows)
   p_array[i] = (char*)malloc(str_ptr-tmp_ptr);
   strncpy(p_array[i], tmp_ptr, str_ptr-tmp_ptr);
   *rows = ++i;
+  return ret_val;
+
 }
 
-void main()
+int main()
 {
   int i=0,ret=0;
   // 1. 二级指针的第1种内存模型
@@ -66,11 +88,16 @@ void main()
   // 2. 二级指针的第2种内存模型
   char buf2[10][30] = {0};
   // 3. 二级指针的第3种内存模型
-  char **buf3 = (char**)malloc(5*sizeof(100));
+  char **buf3 = (char**)malloc(10*sizeof(char **));
   char *str="aaa,bbbb,cccc,dddd";
   int rows=0;
   ret =split_str(str, buf3, &rows);
+  if(ret != 0)
+  {
+    printf("func split_str() error: %d\n", ret);
+    return ret;
 
+  }
   print_array(buf3, rows); 
   printf("rows: %d\n", rows);
   for (i=0;i<rows;i++)
@@ -83,6 +110,7 @@ void main()
   }
   free(buf3);
   buf3=NULL;
+  return ret;
 
 }
 
